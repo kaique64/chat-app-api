b949607f560b51d719bea5f00aadb2425003e7af
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-27 17:36:05 -0300

    create chat notification dto

---
+++ src/main/java/io/chat/app/application/dtos/ChatNotificationDTO.java
@@ -0,0 +1,17 @@
+package io.chat.app.application.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatNotificationDTO {
+    private String id;
+    private String senderId;
+    private String recipientId;
+    private String content;
+}
