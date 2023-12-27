feadd19d6ef64b4a1f46fd5e3a388988d9f25322
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-27 17:35:42 -0300

    add configuration on model mapper

--- src/main/java/io/chat/app/config/ModelMapperConfig.java
+++ src/main/java/io/chat/app/config/ModelMapperConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+
+        return modelMapper;
     }
 }
