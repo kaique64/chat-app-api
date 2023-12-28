0e5db5c8b26e5ff531c8a999fa5c3a995440a842
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-28 17:56:21 -0300

    create test to verify sort in descending order by createdAt field

--- src/test/java/io/chat/app/application/services/GetMessagesServiceTest.java
+++ src/test/java/io/chat/app/application/services/GetMessagesServiceTest.java
@@ -13,11 +13,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -83,7 +82,7 @@ public class GetMessagesServiceTest {
         // Arrange
         String from = message.getSenderId();
         String to = message.getRecipientId();
-        when(chatRepository.findMessagesBySenderAndRecipient(from, to)).thenReturn(messages);
+        when(chatRepository.findMessagesBySenderAndRecipient(from, to, Sort.by(Sort.Direction.DESC, "createdAt"))).thenReturn(messages);
 
         // Act
         List<ChatResponseDTO> result = getMessagesService.getMessagesBySenderAndRecipient(from, to);
@@ -91,6 +90,39 @@ public class GetMessagesServiceTest {
         // Assert
         assertNotNull(result);
         assertEquals(1, result.size());
+    }
+
+    @Test
+    @DisplayName("Should return a list of ChatResponseDTO objects in descending order of creation time")
+    public void test_descending_order_of_creation_time() {
+        // Arrange
+        String from = "sender";
+        String to = "recipient";
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime oneMinuteAgo = now.minusMinutes(1);
+        LocalDateTime twoMinutesAgo = now.minusMinutes(2);
+
+        Chat chat1 = createChat(from, to, "Hello", now);
+        Chat chat2 = createChat(from, to, "Hi", oneMinuteAgo);
+        Chat chat3 = createChat(from, to, "Hey", twoMinutesAgo);
+
+        List<Chat> chatList = Arrays.asList(chat1, chat2, chat3);
+        when(chatRepository.findMessagesBySenderAndRecipient(from, to, Sort.by(Sort.Direction.DESC, "createdAt"))).thenReturn(chatList);
+
+        // Mock the modelMapper.map() method to return non-null ChatResponseDTO objects
+        mockModelMapperMapMethodToReturnNonNullValues();
+
+        // Act
+        List<ChatResponseDTO> result = getMessagesService.getMessagesBySenderAndRecipient(from, to);
+
+        // Assert
+        assertEquals(chatList.size(), result.size());
+        for (int i = 0; i < result.size() - 1; i++) {
+            System.out.println((i));
+            System.out.println(result.get(i).getCreatedAt());
+            assertTrue(result.get(i).getCreatedAt().getMinute() > result.get(i + 1).getCreatedAt().getMinute());
+        }
     }
 
     @Test
