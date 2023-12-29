09a43efcfe9ced535b950afe0b8d7743e0fccd23
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-29 19:16:08 -0300

    create user controller

---
+++ src/main/java/io/chat/app/infra/http/controller/UserController.java
@@ -0,0 +1,27 @@
+package io.chat.app.infra.http.controller;
+
+import io.chat.app.application.user.dtos.CreateUserDTO;
+import io.chat.app.application.user.dtos.UserResponseDTO;
+import io.chat.app.usecases.UserUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserUseCase userUseCase;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid CreateUserDTO userDTO) {
+        return new ResponseEntity<>(userUseCase.create(userDTO), HttpStatus.CREATED);
+    }
+
+}
