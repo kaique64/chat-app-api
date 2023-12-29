f398b00c36869e133b5cc8b890e7846a00d3408a
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-29 16:46:18 -0300

    add getter and setter on DTO

--- src/main/java/io/chat/app/application/user/dtos/CreateUserDTO.java
+++ src/main/java/io/chat/app/application/user/dtos/CreateUserDTO.java
@@ -3,7 +3,14 @@ package io.chat.app.application.user.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CreateUserDTO {
 
     @NotEmpty
