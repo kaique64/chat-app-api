9909a4992c71fee52f10352ab827d51c7aa51cc3
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-29 18:46:42 -0300

    create method to get user by email

--- src/main/java/io/chat/app/infra/database/repository/UserRepository.java
+++ src/main/java/io/chat/app/infra/database/repository/UserRepository.java
@@ -3,5 +3,8 @@ package io.chat.app.infra.database.repository;
 import io.chat.app.infra.database.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
