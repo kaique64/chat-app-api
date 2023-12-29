4b359df1d3cea87ed3f9486c485765e46488a028
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-29 16:49:39 -0300

    add getter and setter

--- src/main/java/io/chat/app/application/user/dtos/UserResponseDTO.java
+++ src/main/java/io/chat/app/application/user/dtos/UserResponseDTO.java
@@ -1,5 +1,12 @@
 package io.chat.app.application.user.dtos;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserResponseDTO {
     private String id;
     private String name;
