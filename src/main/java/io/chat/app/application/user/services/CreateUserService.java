6cc8c3471d3a2973f49ff2b2deeb9ca60eeada29
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-29 18:45:38 -0300

    encode the password before save it and verify if user email already exists

--- src/main/java/io/chat/app/application/user/services/CreateUserService.java
+++ src/main/java/io/chat/app/application/user/services/CreateUserService.java
@@ -1,5 +1,6 @@
 package io.chat.app.application.user.services;
 
+import io.chat.app.application.exceptions.AppException;
 import io.chat.app.application.user.dtos.CreateUserDTO;
 import io.chat.app.application.user.dtos.UserResponseDTO;
 import io.chat.app.application.user.services.interfaces.ICreateUserService;
@@ -7,6 +8,7 @@ import io.chat.app.infra.database.entity.User;
 import io.chat.app.infra.database.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +20,14 @@ public class CreateUserService implements ICreateUserService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserResponseDTO create(CreateUserDTO userDTO) {
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+        if (userRepository.findByEmail(userDTO.getEmail()).isPresent()) throw new AppException("User already exists");
+
         User userCreated = userRepository.insert(modelMapper.map(userDTO, User.class));
 
         return modelMapper.map(userCreated, UserResponseDTO.class);
