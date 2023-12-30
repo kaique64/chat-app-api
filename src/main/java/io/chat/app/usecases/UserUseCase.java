5e781c25f7ddd2c29bfb5b09f5ed58d689850e1b
kaique64 <kaiquehenrique.2005@outlook.com>
2023-12-30 16:25:08 -0300

    use sign in user

--- src/main/java/io/chat/app/usecases/UserUseCase.java
+++ src/main/java/io/chat/app/usecases/UserUseCase.java
@@ -1,16 +1,21 @@
 package io.chat.app.usecases;
 
+import io.chat.app.application.exceptions.AppException;
 import io.chat.app.application.user.dtos.CreateUserDTO;
 import io.chat.app.application.user.dtos.SignInUserDTO;
 import io.chat.app.application.user.dtos.SignInUserResponseDTO;
 import io.chat.app.application.user.dtos.UserResponseDTO;
+import io.chat.app.application.user.services.AuthenticationService;
 import io.chat.app.application.user.services.CreateUserService;
 import io.chat.app.application.user.services.TokenService;
 import io.chat.app.infra.database.entity.User;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,31 +25,14 @@ public class UserUseCase {
     private CreateUserService createUserService;
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private TokenService tokenService;
+    private AuthenticationService authenticationService;
 
     public UserResponseDTO create(CreateUserDTO userDTO) {
         return createUserService.create(userDTO);
     }
 
     public SignInUserResponseDTO signIn(SignInUserDTO userDTO) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDTO.getEmail(), userDTO.getPassword());
-
-        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
-
-        User user = (User) authenticate.getPrincipal();
-
-        String token = tokenService.generateToken(user);
-
-        SignInUserResponseDTO signInUserResponseDTO = new SignInUserResponseDTO();
-        signInUserResponseDTO.setId(user.getId());
-        signInUserResponseDTO.setName(user.getName());
-        signInUserResponseDTO.setEmail(user.getEmail());
-        signInUserResponseDTO.setToken(token);
-
-        return signInUserResponseDTO;
+        return authenticationService.signIn(userDTO);
     }
 
 }
