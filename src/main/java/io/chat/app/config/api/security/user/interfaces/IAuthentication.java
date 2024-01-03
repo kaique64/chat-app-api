15a6a50b19c8ae8522afedc6462c3fb0eb209085
kaique64 <kaiquehenrique.2005@outlook.com>
2024-01-02 23:24:51 -0300

    remove files

--- src/main/java/io/chat/app/config/api/security/user/interfaces/IAuthentication.java
+++
@@ -1,8 +0,0 @@
-package io.chat.app.config.api.security.user.interfaces;
-
-import io.chat.app.config.api.security.user.dtos.SignInUserDTO;
-import io.chat.app.config.api.security.user.dtos.SignInUserResponseDTO;
-
-public interface IAuthentication {
-    SignInUserResponseDTO signIn(SignInUserDTO userDTO);
-}
