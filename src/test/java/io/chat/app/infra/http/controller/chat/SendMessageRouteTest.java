f36bdf1f36f6be583f275d405e2436aaed9a9b30
kaique64 <kaiquehenrique.2005@outlook.com>
2024-01-07 14:50:47 -0300

    remove not used imports

--- src/test/java/io/chat/app/infra/http/controller/chat/SendMessageRouteTest.java
+++ src/test/java/io/chat/app/infra/http/controller/chat/SendMessageRouteTest.java
@@ -2,7 +2,6 @@ package io.chat.app.infra.http.controller.chat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.chat.app.AppApplication;
-import io.chat.app.application.chat.dtos.ChatNotificationDTO;
 import io.chat.app.application.chat.dtos.CreateMessageDTO;
 import io.chat.app.infra.database.entity.User;
 import io.chat.app.infra.database.repository.UserRepository;
@@ -15,17 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
